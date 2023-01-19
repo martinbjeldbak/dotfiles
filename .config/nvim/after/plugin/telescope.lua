a55599df42a2fcaadb002c381c920a45d4abfb38
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-01-19 15:28:23 +1100

    Refresh vim configs (with lua)

---
+++ .config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,6 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fz', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
