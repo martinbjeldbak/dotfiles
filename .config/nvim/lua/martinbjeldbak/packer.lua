cb603e16e2020bb094bfac0969fcebc311948b31
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-04-07 14:56:30 +1000

    Add tresitter-context

--- .config/nvim/lua/martinbjeldbak/packer.lua
+++ .config/nvim/lua/martinbjeldbak/packer.lua
@@ -36,6 +36,7 @@ return require('packer').startup(function(use)
     use('tpope/vim-rails')
 
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter-context')
 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
