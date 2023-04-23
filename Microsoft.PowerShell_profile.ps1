a00790529734e4f47ac0d66876efd7302d773200
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-04-23 12:19:13 +1000

    Add alias for streamlink

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,11 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_modern.omp.json" | Invoke-Expression
+
+function streaml() {
+    param(
+        [string] $streamer
+    )
+
+    Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" "--new-window https://www.twitch.tv/popout/$streamer/chat?popout="
+
+    Start-Process -FilePath "streamlink.exe" "twitch.tv/$streamer best"
+}
