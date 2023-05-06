8e654b15d84896ab59d21e43b7e8e885840572dc
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-05-06 14:16:29 +1000

    Improve nvim tree expected behavior

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -3,13 +3,20 @@ vim.keymap.set("n", "<leader>f", vim.cmd.NvimTreeFindFile)
 
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+  respect_buf_cwd = true,
+    actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+    change_dir = {
+      enable = true,
+      global = true,
+    },
+  },
   view = {
       width = 40,
   },
   renderer = {
     group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
   },
 })
