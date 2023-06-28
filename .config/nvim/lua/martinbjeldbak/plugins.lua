60c2f6ddb8731eb0d181fd78b9c79df421df2561
Martin Bjeldbak Madsen <martin.madsen@anz.com>
2023-06-28 15:07:52 +1000

    Enable some more plugins with theme

--- .config/nvim/lua/martinbjeldbak/plugins.lua
+++ .config/nvim/lua/martinbjeldbak/plugins.lua
@@ -4,6 +4,18 @@ return {
     lazy = false, -- https://github.com/folke/lazy.nvim#-colorschemes
     priority = 1000 , -- make sure to load this before all other start plugins
     config = function()
+      require('catppuccin').setup({
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        integrations = {
+          which_key = true,
+          lsp_trouble = true,
+          harpoon = true,
+        },
+      })
+
       vim.cmd([[colorscheme catppuccin]])
     end,
   },
