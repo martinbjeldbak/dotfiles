e690236e738134ed9edf9979ddc6ddd4d8dfc445
Martin Bjeldbak Madsen <martin.madsen@anz.com>
2023-07-06 11:51:08 +1000

    Fix dependency loading issue between go.nvim and lsp

--- .config/nvim/lua/martinbjeldbak/plugins/languages.lua
+++ .config/nvim/lua/martinbjeldbak/plugins/languages.lua
@@ -3,7 +3,6 @@ return {
     "ray-x/go.nvim",
     dependencies = {  -- optional packages
       "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
     opts = {
@@ -13,6 +12,20 @@ return {
       -- trouble = true,
       luasnip = true,
     },
+    config = function ()
+      require("go").setup()
+    end,
+    init = function ()
+      -- https://github.com/ray-x/go.nvim#run-gofmt--goimport-on-save
+      local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require('go.format').goimport()
+        end,
+        group = format_sync_grp,
+      })
+    end,
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
--- .config/nvim/lua/martinbjeldbak/plugins/lsp.lua
+++ .config/nvim/lua/martinbjeldbak/plugins/lsp.lua
@@ -26,6 +26,7 @@ return {
       "saadparwaiz1/cmp_luasnip",          -- luasnip completion source for nvim-cmp
       "LuaSnip",                           -- snippet engine
       "b0o/schemastore.nvim",
+      "ray-x/go.nvim"                      -- integrates with lsp
     },
     ---@class PluginLspOpts
     opts = {
