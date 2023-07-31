e733e4a8b0c1f494dd11ffd676b072c5b099a2fa
Martin Bjeldbak Madsen <martin.madsen@anz.com>
2023-07-31 15:47:32 +1000

    Reformat, refactor away from setup_handlers

--- .config/nvim/lua/martinbjeldbak/plugins/lsp.lua
+++ .config/nvim/lua/martinbjeldbak/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
       "rafamadriz/friendly-snippets",
     },
     config = function(_, opts)
-      require('luasnip.loaders.from_vscode').lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load()
     end,
   },
 
@@ -18,31 +18,31 @@ return {
     dependencies = {
       { "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
       "mason.nvim",
-      "williamboman/mason-lspconfig.nvim",   -- mason extension for lspconfig
-      "hrsh7th/nvim-cmp",                    -- autocomplete engine
-      "hrsh7th/cmp-buffer",                  -- nvim-cmp source for buffer words
-      "hrsh7th/cmp-path",                    -- nvim-cmp source for filesystem paths.
-      "hrsh7th/cmp-nvim-lsp",                -- show data sent by the language server.
+      "williamboman/mason-lspconfig.nvim", -- mason extension for lspconfig
+      "hrsh7th/nvim-cmp",                 -- autocomplete engine
+      "hrsh7th/cmp-buffer",               -- nvim-cmp source for buffer words
+      "hrsh7th/cmp-path",                 -- nvim-cmp source for filesystem paths.
+      "hrsh7th/cmp-nvim-lsp",             -- show data sent by the language server.
       "hrsh7th/cmp-nvim-lsp-signature-help", -- display function signatures with current parameter emphasized
-      "saadparwaiz1/cmp_luasnip",            -- luasnip completion source for nvim-cmp
-      "LuaSnip",                             -- snippet engine
+      "saadparwaiz1/cmp_luasnip",         -- luasnip completion source for nvim-cmp
+      "LuaSnip",                          -- snippet engine
       "b0o/schemastore.nvim",
     },
     ---@class PluginLspOpts
     opts = {
       diagnostics = {
         virtual_text = true,
-      }
+      },
     },
     ---@param opts PluginLspOpts
     config = function(_, opts)
       -- Heavily inspired by https://github.com/VonHeikemen/nvim-starter/blob/03-lsp/init.lua
-      require('mason').setup({
-        ui = { border = 'rounded' }
+      require("mason").setup({
+        ui = { border = "rounded" },
       })
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      local mlsp = require('mason-lspconfig')
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local mlsp = require("mason-lspconfig")
       local select_opts = { behavior = cmp.SelectBehavior.Select }
 
       -- diagnostics
@@ -50,59 +50,73 @@ return {
 
       mlsp.setup({
         ensure_installed = {
-          'cssls',
-          'dockerls',
-          'docker_compose_language_service',
-          'eslint',
-          'tsserver',
-          'lua_ls',
-          'html',
-          'bashls',
-          'yamlls',
-          'jedi_language_server',
-        }
+          "cssls",
+          "dockerls",
+          "docker_compose_language_service",
+          "eslint",
+          "tsserver",
+          "lua_ls",
+          "html",
+          "bashls",
+          "yamlls",
+          "jedi_language_server",
+        },
       })
 
-      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lsp_attach = function(_, bufnr)
-        local opts = { buffer = bufnr, remap = false }
+      -- Global mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set("n", "vd", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-      end
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+          vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+          vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+        end,
+      })
 
       cmp.setup({
         experimental = {
-          ghost_text = true -- :h cmp-config.experimental.ghost_text
+          ghost_text = true, -- :h cmp-config.experimental.ghost_text
         },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
-          end
+          end,
         },
         sources = {
-          { name = 'path' },                                                            -- gives completions based on the filesystem.
-          { name = 'nvim_lsp',               priority = 100 },                            -- suggestions based on language server
-          { name = 'nvim_lsp_signature_help' },
-          { name = 'buffer',                 keyword_length = 3, max_item_count = 15, priority = 50 }, -- provides suggestions based on the current file
-          { name = 'luasnip',                keyword_length = 2, priority = 3 },        -- it shows snippets loaded by luasnip in the suggestions
+          { name = "path" },                                                      -- gives completions based on the filesystem.
+          { name = "nvim_lsp",               priority = 100 },                    -- suggestions based on language server
+          { name = "nvim_lsp_signature_help" },
+          { name = "buffer",                 keyword_length = 3, max_item_count = 15, priority = 50 }, -- provides suggestions based on the current file
+          { name = "luasnip",                keyword_length = 2, priority = 3 },  -- it shows snippets loaded by luasnip in the suggestions
         },
         formatting = {
-          fields = { 'menu', 'abbr', 'kind' },
+          fields = { "menu", "abbr", "kind" },
           format = function(entry, item)
             local menu_icon = {
-              nvim_lsp = 'λ',
-              luasnip = '⋗',
-              buffer = 'Ω',
-              path = '🖫',
+              nvim_lsp = "λ",
+              luasnip = "⋗",
+              buffer = "Ω",
+              path = "🖫",
             }
             item.menu = menu_icon[entry.source.name]
             return item
@@ -110,134 +124,127 @@ return {
         },
         -- See :help cmp-mapping
         mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete({
-            config = { reason = cmp.ContextReason.Auto },
-          }), { "i", "c" }),
-          ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-          ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+          ["<C-Space>"] = cmp.mapping(
+            cmp.mapping.complete({
+              config = { reason = cmp.ContextReason.Auto },
+            }),
+            { "i", "c" }
+          ),
+          ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+          ["<Down>"] = cmp.mapping.select_next_item(select_opts),
 
-          ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-          ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+          ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
+          ["<C-n>"] = cmp.mapping.select_next_item(select_opts),
 
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
 
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-          ['<C-f>'] = cmp.mapping(function(fallback)
+          ["<C-f>"] = cmp.mapping(function(fallback)
             if luasnip.jumpable(1) then
               luasnip.jump(1)
             else
               fallback()
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
 
-          ['<C-b>'] = cmp.mapping(function(fallback)
+          ["<C-b>"] = cmp.mapping(function(fallback)
             if luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
 
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            local col = vim.fn.col('.') - 1
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            local col = vim.fn.col(".") - 1
 
             if cmp.visible() then
               cmp.select_next_item(select_opts)
-            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+            elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
               fallback()
             else
               cmp.complete()
             end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item(select_opts)
             else
               fallback()
             end
-          end, { 'i', 's' }),
+          end, { "i", "s" }),
         }),
       })
 
-      local lspconfig = require('lspconfig')
-      mlsp.setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup({
-            on_attach = lsp_attach,
-            capabilities = lsp_capabilities,
-          })
-        end,
-        ["lua_ls"] = function()
-          lspconfig.lua_ls.setup({
-            settings = {
-              Lua = {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT',
-                },
-                diagnostics = {
-                  -- Get the language server to recognize the `vim` global
-                  globals = { 'vim' },
-                },
-                workspace = {
-                  -- Make the server aware of Neovim runtime files
-                  library = vim.api.nvim_get_runtime_file("", true),
-                  checkThirdParty = false, -- https://github.com/folke/neodev.nvim/issues/88
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                telemetry = {
-                  enable = false,
-                },
-                completion = {
-                  callSnippet = "Replace",
-                },
-              },
-            }
-          })
-        end,
-        ['gopls'] = function()
-          require("go").setup()                -- https://github.com/ray-x/go.nvim/issues/112#issuecomment-1116715000
-          local cfg = require 'go.lsp'.config() -- config() return the go.nvim gopls setup
-          require('lspconfig').gopls.setup(cfg)
-        end,
-        ['tsserver'] = function()
-          lspconfig.tsserver.setup({
-            settings = {
-              completions = {
-                completeFunctionCalls = true
-              }
-            }
-          })
-        end,
-        ["jsonls"] = function()
-          lspconfig.jsonls.setup({
-            settings = {
-              json = {
-                schemas = require('schemastore').json.schemas(),
-                validate = { enable = true },
-              },
+      local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
             },
-          })
-        end,
-        ["yamlls"] = function()
-          lspconfig.yamlls.setup({
-            settings = {
-              yaml = {
-                schemas = require('schemastore').yaml.schemas(),
-              },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
             },
-          })
-        end,
-        ["lemminx"] = function()
-          lspconfig.lemminx.setup({
-            cmd = { "lemminx",
-              "-Djavax.net.ssl.trustStore=/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home/lib/security/cacerts" }
-          })
-        end,
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false, -- https://github.com/folke/neodev.nvim/issues/88
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
+      })
+      require("go").setup()                 -- https://github.com/ray-x/go.nvim/issues/112#issuecomment-1116715000
+      local gocfg = require("go.lsp").config() -- config() return the go.nvim gopls setup
+
+      lspconfig.gopls.setup(gocfg)
+      lspconfig.tsserver.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          completions = {
+            completeFunctionCalls = true,
+          },
+        },
+      })
+      lspconfig.jsonls.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      })
+      lspconfig.yamlls.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          yaml = {
+            schemas = require("schemastore").yaml.schemas(),
+          },
+        },
+      })
+      lspconfig.lemminx.setup({
+        capabilities = lsp_capabilities,
+        cmd = {
+          "lemminx",
+          "-Djavax.net.ssl.trustStore=/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home/lib/security/cacerts",
+        },
       })
     end,
   },
@@ -327,7 +334,7 @@ return {
       return {
         sources = sources,
         debounce = 1000,
-        default_timeout = 5000
+        default_timeout = 5000,
       }
     end,
   },
@@ -342,10 +349,10 @@ return {
         "stylua",
         "shfmt",
         "black",
-      }
+      },
     },
     config = function(_, opts)
       require("mason").setup(opts)
-    end
-  }
+    end,
+  },
 }
