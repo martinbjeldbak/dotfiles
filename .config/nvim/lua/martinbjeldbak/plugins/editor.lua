aa0769242530244486de573cde56b60b5e006b48
Martin Bjeldbak Madsen <martin.madsen@anz.com>
2023-11-27 10:26:11 +1100

    Abandon dadbod, vpc service controls

--- .config/nvim/lua/martinbjeldbak/plugins/editor.lua
+++ .config/nvim/lua/martinbjeldbak/plugins/editor.lua
@@ -453,24 +453,4 @@ return {
   {
     "ThePrimeagen/vim-be-good"
   },
-
-
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod",                     lazy = true, cmd = { "DB" } },
-      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-    },
-    cmd = {
-      'DBUI',
-      'DBUIToggle',
-      'DBUIAddConnection',
-      'DBUIFindBuffer',
-    },
-    init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-
-      vim.g.db_adapter_bigquery_region = "australia-southeast1"
-    end,
-  },
 }
