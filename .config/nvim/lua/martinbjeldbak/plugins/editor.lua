3b9f0d652b4fecbbfe3916277108db0e6c7ee419
Martin Bjeldbak Madsen <martin.madsen@anz.com>
2023-11-27 10:43:51 +1100

    Remove code folding

--- .config/nvim/lua/martinbjeldbak/plugins/editor.lua
+++ .config/nvim/lua/martinbjeldbak/plugins/editor.lua
@@ -30,58 +30,6 @@ return {
     },
   },
 
-  -- Code folding
-  -- config source: https://github.com/kevinhwang91/nvim-ufo/issues/4#issuecomment-1512772530
-  {
-    'kevinhwang91/nvim-ufo',
-    event = "BufReadPost",
-    dependencies = {
-      'kevinhwang91/promise-async',
-      {
-        "luukvbaal/statuscol.nvim",
-        config = function()
-          local builtin = require("statuscol.builtin")
-          require("statuscol").setup({
-            relculright = true,
-            segments = {
-              { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
-              { text = { "%s" },                  click = "v:lua.ScSa" },
-              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-            },
-          })
-        end,
-      },
-    },
-    init = function()
-      vim.o.foldcolumn = "1" -- '0' is not bad
-      vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    end,
-    keys = {
-      {
-        "zR",
-        function()
-          require('ufo').openAllFolds()
-        end,
-        desc = "Open all folds"
-      },
-      {
-        "zM",
-        function()
-          require('ufo').closeAllFolds()
-        end,
-        desc = "Close all folds"
-      },
-    },
-    opts = {
-      provider_selector = function(bufnr, filetype, buftype)
-        return { 'treesitter', 'indent' }
-      end,
-    },
-  },
-
   -- undotree
   {
     "mbbill/undotree",
