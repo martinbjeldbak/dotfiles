cbecf50fcb0a20de07019dfeda38e5ff67337e15
Martin Bjeldbak Madsen <martin.madsen@anz.com>
2024-01-31 10:07:04 +1100

    Update deps and other minor updates

--- .config/nvim/lua/martinbjeldbak/plugins/lsp.lua
+++ .config/nvim/lua/martinbjeldbak/plugins/lsp.lua
@@ -154,7 +154,7 @@ return {
 
           ["<C-e>"] = cmp.mapping.abort(),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          -- ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
           ["<C-f>"] = cmp.mapping(function(fallback)
             if luasnip.jumpable(1) then
@@ -172,17 +172,17 @@ return {
             end
           end, { "i", "s" }),
 
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            local col = vim.fn.col(".") - 1
+          -- ["<Tab>"] = cmp.mapping(function(fallback)
+          --   local col = vim.fn.col(".") - 1
 
-            if cmp.visible() then
-              cmp.select_next_item(select_opts)
-            elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-              fallback()
-            else
-              cmp.complete()
-            end
-          end, { "i", "s" }),
+          --   if cmp.visible() then
+          --     cmp.select_next_item(select_opts)
+          --   elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+          --     fallback()
+          --   else
+          --     cmp.complete()
+          --   end
+          -- end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item(select_opts)
@@ -296,6 +296,9 @@ return {
             settings = {
               pylsp = {
                 plugins = {
+                  pycodestyle = {
+                    maxLineLength = 120
+                  },
                   pylint = {
                     enabled = false
                   }
