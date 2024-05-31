04e4f19e3ab6b7605ba9cec56613256480163553
Martin Bjeldbak Madsen <martin.madsen@anz.com>
2024-05-31 10:20:54 +1000

    catppuccin nvim transparent background

--- dot_config/nvim/lua/martinbjeldbak/plugins.lua
+++ dot_config/nvim/lua/martinbjeldbak/plugins.lua
@@ -1,27 +1,28 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false, -- https://github.com/folke/lazy.nvim#-colorschemes
-    priority = 1000 , -- make sure to load this before all other start plugins
-    config = function()
-      require('catppuccin').setup({
-        background = {
-          light = "latte",
-          dark = "frappe",
-        },
-        integrations = {
-          harpoon = true,
-          leap = true,
-          lsp_trouble = true,
-          markdown = true,
-          mason = true,
-          mini = true,
-          treesitter_context = true,
-          which_key = true,
-        },
-      })
+	{
+		"catppuccin/nvim",
+		lazy = false, -- https://github.com/folke/lazy.nvim#-colorschemes
+		priority = 1000, -- make sure to load this before all other start plugins
+		config = function()
+			require("catppuccin").setup({
+				background = {
+					light = "latte",
+					dark = "frappe",
+				},
+				transparent_background = true,
+				integrations = {
+					harpoon = true,
+					leap = true,
+					lsp_trouble = true,
+					markdown = true,
+					mason = true,
+					mini = true,
+					treesitter_context = true,
+					which_key = true,
+				},
+			})
 
-      vim.cmd([[colorscheme catppuccin]])
-    end,
-  },
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
