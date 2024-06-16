edb9be7f67e35aaaa546bc16173a78eb54538a27
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2024-06-16 23:46:23 +1000

    minor cosemtic stuff

--- dot_config/nvim/lua/martinbjeldbak/plugins/ui.lua
+++ dot_config/nvim/lua/martinbjeldbak/plugins/ui.lua
@@ -1,37 +1,62 @@
 return {
-  -- dashboard
-  {
-    "glepnir/dashboard-nvim",
-    event = "VimEnter",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
+    -- dashboard
+    {
+        'nvimdev/dashboard-nvim',
+        event = "VimEnter",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = "hyper",
+            config = {
+                week_header = {
+                    enable = true,
+                },
+                shortcut = {
+                    { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                    {
+                        icon = ' ',
+                        icon_hl = '@variable',
+                        desc = 'Files',
+                        group = 'Label',
+                        action = 'Telescope find_files',
+                        key = 'f',
+                    },
+                    {
+                        desc = ' dotfiles',
+                        group = 'Number',
+                        action = 'Telescope dotfiles',
+                        key = 'd',
+                    },
+                },
+            },
+        },
     },
-  },
 
-  -- Make it rain
-  {
-    "eandrju/cellular-automaton.nvim",
-    cmd = "CellularAutomaton",
-    keys = {
-      { "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Contemplate life" }
+    -- Make it rain
+    {
+        "eandrju/cellular-automaton.nvim",
+        cmd = "CellularAutomaton",
+        keys = {
+            { "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Contemplate life" }
+        },
     },
-  },
 
-  -- Statusline
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-web-devicons",
-    },
-    opts = {
-      options = {
-        theme = "catppuccin",
-      },
-      extensions = {
-        "fugitive",
-        "fzf",
-        "nvim-tree",
-      },
-    },
-  }
+    -- Statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-web-devicons",
+        },
+        opts = {
+            options = {
+                theme = "catppuccin",
+            },
+            extensions = {
+                "fugitive",
+                "fzf",
+                "nvim-tree",
+            },
+        },
+    }
 }
