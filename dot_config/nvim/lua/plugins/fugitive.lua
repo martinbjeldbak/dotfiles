039e35a37a63dcf1b3c7270884a1fdc9a7138b0d
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2024-08-15 11:00:43 +1000

    nvim: revamp to use kickstart as base

---
+++ dot_config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,12 @@
+return {
+	"tpope/vim-fugitive",
+	keys = {
+		{ "<leader>gs", vim.cmd.Git, desc = "Git status" },
+		{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
+		{ "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
+	},
+	cmd = { "G", "GBrowse" },
+	dependencies = {
+		{ "tpope/vim-rhubarb", dependencies = { "tyru/open-browser.vim" } },
+	},
+}
