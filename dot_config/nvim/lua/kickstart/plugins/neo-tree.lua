1c4fdecaea535dcc0744ddbd9770289b5ed65d8b
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2024-08-16 14:57:06 +1000

    add kickstart plugins

---
+++ dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal right<CR>", desc = "NeoTree reveal" },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
+}
