683d5dfd1e66fadbc1605c3c002329a337257f78
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2024-11-08 11:20:25 +1100

    vim: support opening in system browse neo-tree

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
 	},
 	opts = {
 		filesystem = {
+			use_libuv_file_watcher = true,
 			filtered_items = {
 				visible = true,
 				hide_dotfiles = false,
@@ -21,11 +22,33 @@ return {
 			window = {
 				mappings = {
 					["\\"] = "close_window",
+					["o"] = "system_open",
 				},
 			},
 			follow_current_file = {
 				enabled = true,
 			},
+		},
+		commands = {
+			-- source: https://github.com/nvim-neo-tree/neo-tree.nvim/wiki/Recipes#open-with-system-viewer
+			system_open = function(state)
+				local node = state.tree:get_node()
+				local path = node:get_id()
+				-- macOs: open file in default application in the background.
+				vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
+				-- Linux: open file in default application
+				vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+
+				-- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+				local p
+				local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+				if lastSlashIndex then
+					p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
+				else
+					p = path -- If no slash found, return original path
+				end
+				vim.cmd("silent !start explorer " .. p)
+			end,
 		},
 	},
 }
