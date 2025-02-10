86b51456da0fcd8605648690dc8fd8a76db415b0
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2025-02-11 09:59:59 +1100

    fix: remove unused keybind

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -151,7 +151,6 @@ return {
 					Snacks.toggle
 						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
 						:map("<leader>uc")
-					Snacks.toggle.treesitter():map("<leader>uT")
 					Snacks.toggle
 						.option("background", { off = "light", on = "dark", name = "Dark Background" })
 						:map("<leader>ub")
