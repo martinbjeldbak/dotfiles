8aee3e31eda84f8b9046f3aebafa61e260c2bafc
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2025-02-17 16:23:42 +1100

    fix(nvim): spectre adding -E at end of files on macos

--- dot_config/nvim/lua/plugins/nvim-spectre.lua
+++ dot_config/nvim/lua/plugins/nvim-spectre.lua
@@ -1,6 +1,18 @@
 return -- search/replace in multiple files
 {
 	"nvim-pack/nvim-spectre",
+	opts = {
+		replace_engine = { -- https://github.com/nvim-pack/nvim-spectre/issues/118#issuecomment-1531683211
+			["sed"] = {
+				cmd = "sed",
+				args = {
+					"-i",
+					"",
+					"-E",
+				},
+			},
+		},
+	},
 	keys = {
 		{
 			"<leader>sR",
